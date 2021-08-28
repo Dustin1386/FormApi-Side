3d46c278902ea6e3abc4d48c6318c583831541ff
dustin <dustinchesnutt@yahoo.com>
2021-08-27 23:31:43 -0700

    added folder and reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FactFormComponent } from './fact-form/fact-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FactFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/fact-form/fact-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fact-form',
+  templateUrl: './fact-form.component.html',
+  styleUrls: ['./fact-form.component.css']
+})
+export class FactFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
